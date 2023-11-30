c75cb4ba4fb148371240c3abfabc3639062365f7
yonatan-h <yon019hl@gmail.com>
2023-11-30 11:14:19 +0300

    copy text from figma

--- upsurge/src/pages/ForYou.jsx
+++ upsurge/src/pages/ForYou.jsx
@@ -1,3 +1,46 @@
 export default function WhatCanWeDoForYou() {
-  return <p>this and this</p>
+  const developments = [
+    {
+      image: '',
+      title: 'Web Development',
+      content: `We specialize in providing top-notch web
+development services to help businesses 
+establish a strong online presence.`,
+    },
+    {
+      image: '',
+      title: 'Mobile Development',
+      content: `We specialize in providing top-notch web
+development services to help businesses 
+establish a strong online presence.`,
+    },
+    {
+      image: '',
+      title: 'Software Development',
+      content: `We specialize in providing top-notch web
+development services to help businesses 
+establish a strong online presence.`,
+    },
+    {
+      image: '',
+      title: 'E-Commerce Dev’t',
+      content: `We specialize in providing top-notch web
+development services to help businesses 
+establish a strong online presence.`,
+    },
+    {
+      image: '',
+      title: 'Graphics Design',
+      content: `We specialize in providing top-notch web
+development services to help businesses 
+establish a strong online presence.`,
+    },
+  ]
+
+  return (
+    <section>
+      <h2>WHAT CAN WE DO FOR YOU</h2>
+      <p>We develop softwares that helps millions of professionals</p>
+    </section>
+  )
 }
