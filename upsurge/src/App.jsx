1c70409a12c8444dd1846fc0933ce8441e6a19a8
yonatan-h <yon019hl@gmail.com>
2023-11-30 10:40:49 +0300

    clean and connect to app.js

--- upsurge/src/App.jsx
+++ upsurge/src/App.jsx
@@ -1,11 +1,11 @@
-import Home from "./components/home/home";
+import Home from './components/home/home'
 
 function App() {
   return (
     <main className="bg-[#050D1F] w-screen h-screen">
       <Home />
     </main>
-  );
+  )
 }
 
 export default App;
---
+++ upsurge/src/pages/ForYou.jsx
@@ -0,0 +1,3 @@
+export default function WhatCanWeDoForYou() {
+  return <p>this and this</p>
+}
