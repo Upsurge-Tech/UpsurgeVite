8cb7488a7bc7d9b1dc9dbddfa3a827d843e4867c
yonatan-h <yon019hl@gmail.com>
2023-12-01 18:32:16 +0300

    copy text from figma

--- upsurge/src/components/OurValues/index.jsx
+++ upsurge/src/components/OurValues/index.jsx
@@ -1,3 +1,41 @@
 export default function OurValues() {
-  return <p>hello</p>
+    const values = [
+        {
+            image:'',
+            title:'Innovation',
+            content:`Building an enterprise doesn't need to be a nightmare or cost 
+            you thousands. Felix is purpose built.`
+        },
+        {
+            image:'',
+            title:'Growth',
+            content:`Building an enterprise doesn't need to be a nightmare or cost 
+            you thousands. Felix is purpose built.`
+        },
+        {
+            image:'',
+            title:'Ownership',
+            content:`Building an enterprise doesn't need to be a nightmare or cost 
+            you thousands. Felix is purpose built.`
+        },
+        {
+            image:'',
+            title:'Team Work',
+            content:`Building an enterprise doesn't need to be a nightmare or cost 
+            you thousands. Felix is purpose built.`
+        },
+        {
+            image:'',
+            title:'Commitment',
+            content:`Building an enterprise doesn't need to be a nightmare or cost 
+            you thousands. Felix is purpose built.`
+        },
+        {
+            image:'',
+            title:'Positivity',
+            content:`Building an enterprise doesn't need to be a nightmare or cost 
+            you thousands. Felix is purpose built.`
+        },
+    ]
+  return <h2>Our Values</h2>
 }
