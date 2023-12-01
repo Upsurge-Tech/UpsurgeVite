fbe07c30ae549b3d77cf24fd28bb2caee207cbfa
yonatan-h <yon019hl@gmail.com>
2023-12-01 20:24:51 +0300

    grid layout finish

--- upsurge/src/components/OurValues/index.jsx
+++ upsurge/src/components/OurValues/index.jsx
@@ -1,55 +1,71 @@
 export default function OurValues() {
   const values = [
     {
-      image: '',
+      color: '#7f7dfd',
       title: 'Innovation',
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      image: '',
+      color: '#3ad5ff',
       title: 'Growth',
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      image: '',
+      color: '#e544ed',
       title: 'Ownership',
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      image: '',
+      color: '#8b7259',
       title: 'Team Work',
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      image: '',
+      color: '#8c5b5b',
       title: 'Commitment',
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      image: '',
+      color: '#5f8158',
       title: 'Positivity',
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
   ]
   return (
-    <section className="bg-[black]">
-      <h2>Our Values</h2>
-      <div>
-        <StyleSquare color={'#807dfd'} />
-      </div>
-      {/* <StyleSquare color={'#39d4ff'} />
-      <StyleSquare color={'#e644ee'} />
+    <section className="bg-[black] text-white px-[2rem]">
 
-      <StyleSquare color={'#8c7359'} />
-      <StyleSquare color={'#8c5a5a'} />
-      <StyleSquare color={'#608359'} /> */}
+      <div className="max-w-[1070px] m-auto ">
+         
+        <h2>Our Values</h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[150px] ">
+          {values.map(({ color, title, content }) => (
+            <ValueCard
+              key={title}
+              color={color}
+              title={title}
+              content={content}
+            />
+          ))}
+        </div>
+      </div>
     </section>
+  )
+}
+
+function ValueCard({ color, title, content }) {
+  return (
+    <div className="flex flex-col items-center text-center gap-[13px]">
+      <StyleSquare color={color} />
+      <h3 className="text-[30px]">{title}</h3>
+      <p className="font-[300]">{content}</p>
+    </div>
   )
 }
 
