30b57c9083bbe8b448667fa96d03a2f564f7f404
Feven Tolla <88964869+Feventolla@users.noreply.github.com>
2023-12-04 10:18:57 +0300

    Whyus (#6)
    * whyus(#2)
    
    * final
    
    * final

--- upsurge/src/App.jsx
+++ upsurge/src/App.jsx
@@ -1,6 +1,7 @@
-import Home from './components/home/home.jsx'
-import Services from './components/Services'
-import OurValues from './components/OurValues'
+import Home from "./components/home/home.jsx";
+import Services from "./components/Services";
+import OurValues from "./components/OurValues";
+import Whyus from "./components/whyus/whyus.jsx";
 
 function App() {
   return (
@@ -8,8 +9,9 @@ function App() {
       <Home />
       <Services />
       <OurValues />
+      <Whyus />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
