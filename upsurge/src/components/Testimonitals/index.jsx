7bd00d44f656824435f404b424aab5961b391bd6
Yonatan <yon019hl@gmail.com>
2023-12-12 10:53:32 +0300

    implement testimonial card

--- upsurge/src/components/Testimonitals/index.jsx
+++ upsurge/src/components/Testimonitals/index.jsx
@@ -1,22 +1,66 @@
 import { createRef, useState, useRef } from 'react'
+import profileImage from '../../assets/testimonials-and-partners/profile.png'
 
 export default function Testimonials() {
   //Assume >= 4 testimonialsx
-  const [contents, setContents] = useState([
-    '-',
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    '--',
-  ])
+  const testimonials = [
+    {
+      id: 0,
+      content: `Designers need to have a strong understanding of the principles of design in order to create effective solutions. They must also be aware of the latest trends and technologies so that they can stay ahead of the curve.`,
+      name: 'Jane Doe',
+      description: 'CEO of Inkyy.com',
+      profileImage: profileImage,
+      rating: 4,
+    },
+    {
+      id: 1,
+      content: `Designers need to have a strong understanding of the principles of design in order to create effective solutions. They must also be aware of the latest trends and technologies so that they can stay ahead of the curve.`,
+      name: 'Jane Doe',
+      description: 'CEO of Inkyy.com',
+      profileImage: profileImage,
+      rating: 3,
+    },
+    {
+      id: 2,
+      content: `Designers need to have a strong understanding of the principles of design in order to create effective solutions. They must also be aware of the latest trends and technologies so that they can stay ahead of the curve.`,
+      name: 'Jane Doe',
+      description: 'CEO of Inkyy.com',
+      profileImage: profileImage,
+      rating: 5,
+    },
+
+    {
+      id: 3,
+      content: `Designers need to have a strong understanding of the principles of design in order to create effective solutions. They must also be aware of the latest trends and technologies so that they can stay ahead of the curve.`,
+      name: 'Jane Doe',
+      description: 'CEO of Inkyy.com',
+      profileImage: profileImage,
+      rating: 5,
+    },
+
+    {
+      id: 4,
+      content: `Designers need to have a strong understanding of the principles of design in order to create effective solutions. They must also be aware of the latest trends and technologies so that they can stay ahead of the curve.`,
+      name: 'Jane Doe',
+      description: 'CEO of Inkyy.com',
+      profileImage: '',
+      rating: 5,
+    },
+
+    {
+      id: 5,
+      content: `Designers need to have a strong understanding of the principles of design in order to create effective solutions. They must also be aware of the latest trends and technologies so that they can stay ahead of the curve.`,
+      name: 'Jane Doe',
+      description: 'CEO of Inkyy.com',
+      profileImage: '',
+      rating: 5,
+    },
+  ]
+
   const [index, setIndex] = useState(1)
 
   const parentRef = useRef()
-  const refs = contents.map(() => createRef())
+  const refs = testimonials.map(() => createRef())
   const updateIndex = () => {
     if (!parentRef.current) return
 
@@ -41,37 +85,73 @@ export default function Testimonials() {
   }
 
   return (
-    <div>
+    <section className='bg-black'>
       <div
-        className='bg-[red] flex overflow-scroll snap-x snap-mandatory gap-5'
+        className=' flex overflow-scroll snap-x snap-mandatory gap-5'
         onScroll={updateIndex}
         ref={parentRef}
       >
-        {contents.map((content, i) => (
+        {testimonials.map((testimonial, i) => (
           <div
-            className='flex-shrink-0 snap-center w-[500px] h-[200px] bg-[green]'
+            className='flex-shrink-0 snap-center w-[500px] '
             ref={refs[i]}
-            key={content}
+            key={testimonial.id}
           >
-            <TestimonialCard content={content} />
+            <TestimonialCard testimonial={testimonial} />
           </div>
         ))}
       </div>
 
       <div>
-        {contents.slice(1, contents.length - 1).map((content, i) => (
-          <button className={i + 1 === index ? 'bg-black' : ''} key={content}>
-            o
-          </button>
-        ))}
+        {testimonials
+          .slice(1, testimonials.length - 1)
+          .map((testimonial, i) => (
+            <button
+              className={i + 1 === index ? 'bg-black' : ''}
+              key={testimonial.id}
+            >
+              o
+            </button>
+          ))}
+      </div>
+    </section>
+  )
+}
+
+function TestimonialCard({ testimonial }) {
+  return (
+    <div className='bg-[#18181C] rounded-[20px] text-white p-[40px] text-[18px] flex flex-col gap-[32px] font-Poppins'>
+      <p className='leading-[38px]'>{testimonial.content}</p>
+      <hr className='border-[#404047]' />
+      <div className='flex justify-between items-center'>
+        <div className='flex gap-[20px]'>
+          <img src={testimonial.profileImage} alt='' className='w-[43px] h-[57px] rounded-[52px]' />
+          <div>
+            <p className='font-[600] text-[20px]'>{testimonial.name}</p>
+            <p className='text-[#9E9E9E] text-[14px]'>{testimonial.description}</p>
+          </div>
+        </div>
+        <Stars rating={testimonial.rating} />
       </div>
     </div>
   )
 }
 
-function TestimonialCard({ content }) {
+function BrightStar() {
+return <svg width="19" height="22" viewBox="0 0 19 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M9.62119 17.0062L4.11902 21.8479L6.06041 13.7342L0.719238 8.61311L7.42145 8.44027L9.62119 0.433105L11.8228 8.44027L18.525 8.61311L13.182 13.7342L15.1234 21.8479L9.62119 17.0062Z" fill="#167796"/>
+</svg>
+}
+function DarkStar() {
+  return <div className='opacity-30'><BrightStar/></div>
+}
+
+function Stars({ rating }) {
   return (
-    //add a scroll end event listener to update the index
-    <p>{content}</p>
+    <div className='flex gap-[5px]'>
+      {[1, 2, 3, 4, 5].map((num) =>
+        num <= rating ? <BrightStar key={num} /> : <DarkStar key={num} />
+      )}
+    </div>
   )
 }
