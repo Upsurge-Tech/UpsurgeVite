2f41cfd915e3789cc76d44a31af12aad97a36470
Yonatan <yon019hl@gmail.com>
2023-12-16 22:49:28 +0300

    add boop effect on service cards

--- upsurge/src/components/Services/DevelopmentCard.jsx
+++ upsurge/src/components/Services/DevelopmentCard.jsx
@@ -5,28 +5,38 @@ import { useEffect } from 'react'
 export default function DevelopmentCard({ development, gridArea }) {
   const { image, title, content } = development
   const controls = useAnimation()
-  const [ref, inView] = useInView({ rootMargin: '-20%'})
+  const [ref, inView] = useInView({ rootMargin: '-20%' })
   const variants = {
-    hidden: { opacity: 0, scale: 0.8 },
+    hidden: { opacity: 0, scale: 0.5 },
     visible: {
       opacity: 1,
       scale: 1,
-      transition: { type: 'tween' },
+    },
+    boop: {
+      scale: 1.05,
     },
   }
+
   useEffect(() => {
-    if (inView) controls.start('visible')
+    controls.set('hidden')
+  }, [controls])
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible')
+    }
   }, [controls, inView])
 
   return (
     <motion.div
       ref={ref}
       variants={variants}
-      initial="hidden"
+      initial='visible'
       animate={controls}
+      whileHover='boop'
       className={`
         bg-gradient-to-br from-[#9f9f9f] to-[#7e7e7e]  via-[#232323] 
-        p-[2px] rounded-[10px] flex flex-col
+        p-[2px] rounded-[10px] flex flex-col z-10
         `}
       style={{ gridArea }}
     >
@@ -37,9 +47,9 @@ export default function DevelopmentCard({ development, gridArea }) {
             bg-gradient-to-br from-[#404040] to-black
     `}
       >
-        <img src={image} className="w-[80px] h-[80px]" />
-        <h3 className="text-[1.5rem] leading-[40px] text-white">{title}</h3>
-        <p className="font-[600] text-[#948989]">{content}</p>
+        <img src={image} className='w-[80px] h-[80px]' />
+        <h3 className='text-[1.5rem] leading-[40px] text-white'>{title}</h3>
+        <p className='font-[600] text-[#948989]'>{content}</p>
       </div>
     </motion.div>
   )
