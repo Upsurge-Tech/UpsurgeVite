0bdc31e90239bc4848fdc649f1d5131bf4db3006
fev <feventolla99@gmail.com>
2023-12-28 19:41:39 +0300

    testimonial-slider

--- upsurge/src/components/Testimonitals/TestimonialDot.jsx
+++ upsurge/src/components/Testimonitals/TestimonialDot.jsx
@@ -3,9 +3,9 @@ export default function TestimonialDot({ active, onClick }) {
     <button
       className={`
           w-[16px] h-[16px] rounded-[100%]
-          ${active ? 'bg-[#00ffff]' : 'bg-[#18171c]'}
+          ${active ? "bg-[#00ffff]" : "bg-[#5d5872]"}
         `}
       onClick={onClick}
     ></button>
-  )
+  );
 }
