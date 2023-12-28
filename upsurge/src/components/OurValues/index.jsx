7607fb8c5799814665ecb20ba48fc2c9431bb93c
fev <feventolla99@gmail.com>
2023-12-28 15:54:11 +0300

    values-layout

--- upsurge/src/components/OurValues/index.jsx
+++ upsurge/src/components/OurValues/index.jsx
@@ -1,52 +1,52 @@
-import ValueCard from './ValueCard'
+import ValueCard from "./ValueCard";
 
 export default function OurValues() {
   const values = [
     {
-      color: '#7f7dfd',
-      title: 'Innovation',
+      color: "#7f7dfd",
+      title: "Innovation",
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      color: '#3ad5ff',
-      title: 'Growth',
+      color: "#3ad5ff",
+      title: "Growth",
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      color: '#e544ed',
-      title: 'Ownership',
+      color: "#e544ed",
+      title: "Ownership",
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      color: '#8b7259',
-      title: 'Team Work',
+      color: "#8b7259",
+      title: "Team Work",
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      color: '#8c5b5b',
-      title: 'Commitment',
+      color: "#8c5b5b",
+      title: "Commitment",
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
     {
-      color: '#5f8158',
-      title: 'Positivity',
+      color: "#5f8158",
+      title: "Positivity",
       content: `Building an enterprise doesn't need to be a nightmare or cost 
             you thousands. Felix is purpose built.`,
     },
-  ]
+  ];
   return (
     <section className="bg-[black] text-white px-[2rem] py-[100px] overflow-hidden">
       <div className="max-w-[1000px] m-auto ">
-        <div className="flex items-center gap-[63px] pb-[100px]">
+        <div className="flex md:flex-col items-center gap-[63px] pb-[100px]">
           <h2 className="text-4xl md:text-5xl ">
-            Our <span className="text-[#5cc0e0]">Values</span>{' '}
+            Our <span className="text-[#5cc0e0]">Values</span>{" "}
           </h2>
-          <div className="flex-1 bg-gradient-to-r from-[#6adbfe] to-white h-[3px]"></div>
+          <div className="hidden md:block lg:h-[450px] lg:w-0 lg:border-r-2 border-b-2 w-[45em] bg-gradient-to-r from-[#6adbfe] to-white h-[3px]"></div>
         </div>
 
         <div
@@ -65,5 +65,5 @@ export default function OurValues() {
         </div>
       </div>
     </section>
-  )
+  );
 }
